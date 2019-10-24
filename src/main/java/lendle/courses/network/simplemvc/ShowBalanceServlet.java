f6a628b2bffdb9a26c695383203090faa67fc9e7
user <user@192.168.1.21>
2019-10-24 17:03:30 +0800

    1024

---
+++ src/main/java/lendle/courses/network/simplemvc/ShowBalanceServlet.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lendle.courses.network.simplemvc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author user
+ */
+@WebServlet(name = "ShowBalanceServlet", urlPatterns = {"/ShowBalanceServlet"})
+public class ShowBalanceServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String id=request.getParameter("id");
+        BankCustomer customer=BankCustomer.getCustomer(id);
+        if(customer==null){
+        }else if(customer.getBalance()<0){
+            request.setAttribute("customer", customer);
+            RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/bank-account/NegativeBalance.jsp");
+            rd.forward(request,response);
+        }else if(customer.getBalance()>10000){
+            request.setAttribute("customer", customer);
+            RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/bank-account/HighBalance.jsp");
+            rd.forward(request,response);
+        }else{
+        }
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
